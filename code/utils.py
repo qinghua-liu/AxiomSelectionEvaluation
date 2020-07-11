0094d54f5966530b62409da9c1fd1004cfc887bf
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-07-10 22:49:10 -0400

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -1,4 +1,37 @@
+import csv
+import logging
+
+
 def read_lines(file_path):
     with open(file_path, 'r') as f:
         lines = f.read().splitlines()
     return lines
+
+
+def write_problem(thm, ranking, statements, problem_file):
+    with open(problem_file, "w+") as f:
+        f.write(statements[thm].replace("axiom", "conjecture") + "\n")
+        f.writelines("\n".join([statements[prem] for prem in ranking]))
+
+
+def write_ranking_to_csv(prem2score, csv_file):
+    with open(csv_file, "w+") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerows(prem2score)
+
+
+def set_recorder(name, logfile):
+    recorder = logging.getLogger(name)
+    recorder.setLevel(logging.INFO)
+    rf_handler = logging.StreamHandler()
+    rf_handler.setLevel(logging.INFO)
+    rf_handler.setFormatter(logging.Formatter(
+        "%(asctime)s - %(name)s - %(message)s"))
+
+    f_handler = logging.FileHandler(logfile)
+    f_handler.setLevel(logging.INFO)
+    f_handler.setFormatter(logging.Formatter(
+        "%(asctime)s - %(name)s - %(message)s"))
+    recorder.addHandler(rf_handler)
+    recorder.addHandler(f_handler)
+    return recorder
