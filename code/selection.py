24cdb38d3a23ba708fca02c7780aac9191bdf20b
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-07-12 19:14:59 -0400

    Update selection.py

--- code/selection.py
+++ code/selection.py
@@ -1,7 +1,8 @@
 from distance import distance_between_formulas, compute_noninfinity
 
 
-def problem_weights(theorem, available_prems, symbol_features, weight="default"):
+def problem_weights(theorem, available_prems,
+                    symbol_features, weight="default"):
     if weight == "default":
         symbol_set = set()
         for name in available_prems + [theorem]:
@@ -14,7 +15,7 @@ def problem_weights(theorem, available_prems, symbol_features, weight="default")
         symbol2frequency = dict()
         for name in available_prems + [theorem]:
             for sym in set(symbol_features[name]):
-                if not sym in symbol2frequency:
+                if sym not in symbol2frequency:
                     symbol2frequency[sym] = 1
                 else:
                     symbol2frequency[sym] += 1
