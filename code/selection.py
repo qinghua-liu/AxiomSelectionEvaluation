b3494ddd560098cfa9e4b47625f6f1334e251d61
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-07-12 15:03:07 -0400

    Update selection.py

--- code/selection.py
+++ code/selection.py
@@ -1,8 +1,8 @@
 from distance import distance_between_formulas, compute_noninfinity
 
 
-def problem_weights(theorem, available_prems, symbol_features, type="default"):
-    if type == "default":
+def problem_weights(theorem, available_prems, symbol_features, weight="default"):
+    if weight == "default":
         symbol_set = set()
         for name in available_prems + [theorem]:
             symbol_set.update(symbol_features[name])
@@ -10,14 +10,28 @@ def problem_weights(theorem, available_prems, symbol_features, type="default"):
         f_weights = dict(zip(list(symbol_set), [2.0] * len(symbol_set)))
         v_weight = 1.0
         return f_weights, v_weight
+    if weight == "frequency":
+        symbol2frequency = dict()
+        for name in available_prems + [theorem]:
+            for sym in set(symbol_features[name]):
+                if not sym in symbol2frequency:
+                    symbol2frequency[sym] = 1
+                else:
+                    symbol2frequency[sym] += 1
+        f_weights = {
+            sys: symbol2frequency[sys] / (len(available_prems) + 1)
+            for sys in symbol2frequency}
+        v_weight = sum(list(f_weights.values())) / len(f_weights)
+        return f_weights, v_weight
 
 
 def problem_ranking(theorem, available_prems, atom_features,
-                    f_weights, v_weight):
+                    f_weights, v_weight, metric, combine):
     distances = []
     for prem in available_prems:
         d = distance_between_formulas(
-            atom_features[theorem], atom_features[prem], f_weights, v_weight)
+            atom_features[prem], atom_features[theorem],
+            f_weights, v_weight, metric, combine)
         distances.append(d)
 
     prem2score = dict(zip(available_prems, distances))
