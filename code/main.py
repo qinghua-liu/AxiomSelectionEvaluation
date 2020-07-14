ce7395d235072b3c9aeff362b0429c6592b08e0a
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-07-13 20:00:22 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ from data_structure import Atom_Features, Symbol_Features, \
     Chronology, Problem_Order, Statements
 from selection import problem_ranking, problem_weights
 from utils import write_ranking_to_csv, set_recorder
-from atp import proofs_from_ranking
+from atp import E_proofs_from_ranking, Vampire_proofs_from_ranking
 
 
 def set_parameters():
@@ -15,8 +15,10 @@ def set_parameters():
                         help="the root path to save ranking csv file")
     params.add_argument("--problem_dir", type=str, default="../problem/symmetry_combine",
                         help="the root path to save problems")
-    params.add_argument("--output_dir", type=str, default="../output/symmetry_combine",
-                        help="the root paht to save outputs")
+    params.add_argument("--E_output_dir", type=str, default="../E_output/symmetry_combine",
+                        help="the root paht to save E outputs")
+    params.add_argument("--Vampire_output_dir", type=str,
+                        default="../Vampire_output/symmetry_combine")
     params.add_argument("--weight", type=str, default="default",
                         help="the weight for symbols, {default, frequency}")
     params.add_argument("--metric", type=str, default="symmetry",
@@ -28,16 +30,19 @@ def set_parameters():
 
 
 def process(thm, chronology, statements, atom_features,
-            symbol_features, weight, metric, combine, problem_dir, output_dir,
-            ranking_dir, recorder):
+            symbol_features, slice_list, weight, metric, combine,
+            problem_dir, E_output_dir, Vampire_output_dir, ranking_dir):
     available_prems = chronology.available_premises(thm)
     f_weights, v_weight = problem_weights(
         thm, available_prems, symbol_features, weight)
     prem2score = problem_ranking(thm, available_prems, atom_features,
                                  f_weights, v_weight, metric, combine)
     write_ranking_to_csv(prem2score, os.path.join(ranking_dir, thm))
-    proofs_from_ranking(thm, prem2score, statements,
-                        problem_dir, output_dir, recorder)
+    ranking = [pair[0] for pair in prem2score]
+    E_proofs_from_ranking(thm, ranking, slice_list,
+                          statements, problem_dir, E_output_dir)
+    Vampire_proofs_from_ranking(thm, ranking, slice_list, statements,
+                                problem_dir, Vampire_output_dir)
 
 
 if __name__ == "__main__":
@@ -53,14 +58,17 @@ if __name__ == "__main__":
         os.makedirs(args.ranking_dir)
     if not os.path.exists(args.problem_dir):
         os.makedirs(args.problem_dir)
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
+    if not os.path.exists(args.E_output_dir):
+        os.makedirs(args.E_output_dir)
+    if not os.path.exists(args.Vampire_output_dir):
+        os.makedirs(args.Vampire_output_dir)
 
-    recorder = set_recorder("E", os.path.join(args.ranking_dir, "record.log"))
-    Parallel(n_jobs=5)(delayed(process)(thm, chronology, statements,
-                                        atom_features, symbol_features,
-                                        args.weight, args.metric,
-                                        args.combine, args.problem_dir,
-                                        args.output_dir, args.ranking_dir,
-                                        recorder)
-                       for thm in tqdm(problem_order[:10]))
+    slice_list = [32, 64, 128, 256, 512, 1024]
+    Parallel(n_jobs=10)(delayed(process)(thm, chronology, statements,
+                                         atom_features, symbol_features,
+                                         slice_list, args.weight,
+                                         args.metric, args.combine,
+                                         args.problem_dir, args.E_output_dir,
+                                         args.Vampire_output_dir,
+                                         args.ranking_dir)
+                        for thm in tqdm(problem_order))
