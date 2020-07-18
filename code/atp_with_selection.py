9233c0ca34f2564aab813e758f00bd55cbaefdb0
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-07-17 20:29:39 -0400

    Update atp_with_selection.py

--- code/atp_with_selection.py
+++ code/atp_with_selection.py
@@ -4,6 +4,7 @@ from atp import run_E_prover, run_Vampire_prover
 from utils import write_problem
 from data_structure import Statements, Chronology, Problem_Order
 from joblib import Parallel, delayed
+from tqdm import tqdm
 
 
 PATH_TO_E = "/exp/home/tptp/Systems/E---2.5/eprover"
@@ -45,45 +46,50 @@ def run_Vampire_prover_with_selection(input_file, output_file, cpu_time):
     output.close()
 
 
-def process(thm, statements, chronology, problem_dir,
-            E_no_selection, E_selection,
-            Vampire_no_selection, Vampire_selection):
-    available_prems = chronology.available_premises(thm)
-    problem_file = os.path.join(problem_dir, thm)
-    write_problem(thm, available_prems, statements, problem_file)
-    E_no_selection_file = os.path.join(E_no_selection, thm)
-    run_E_prover(problem_file, E_no_selection_file, 60)
-    Vampire_no_selection_file = os.path.join(Vampire_no_selection, thm)
-    run_Vampire_prover(problem_file, Vampire_no_selection_file, 60)
-    E_selection_file = os.path.join(E_selection, thm)
-    run_E_prover_with_selection(problem_file, E_selection_file, 60)
-    Vampire_selection_file = os.path.join(Vampire_selection, thm)
-    run_Vampire_prover_with_selection(
-        problem_file, Vampire_selection_file, 60)
+def run_Vampire_prover_with_only_selection(input_file, output_file):
+    output = open(output_file, "w+")
+    subprocess.call([
+        PATH_TO_Vampire,
+        "--mode",
+        "axiom_selection",
+        "--output_axiom_names",
+        "on",
+        input_file],
+        stdout=output, stderr=open(os.devnull, "w"))
+
+
+def run_E_prover_with_only_selection(input_file, output_file):
+    output = open(output_file, "w+")
+    subprocess([
+        PATH_TO_E,
+        "--sine=Auto",
+        "--tstp-format",
+        "—prune",
+        input_file],
+        stdout=output, stderr=open(os.devnull, "w"))
+
+
+def process(thm, problem_dir,
+            selected_problem_dir, output_dir):
+    input_file = os.path.join(problem_dir, thm)
+    selected_file = os.path.join(selected_problem_dir, thm)
+    run_Vampire_prover_with_only_selection(input_file, selected_file)
+    output_file = os.path.join(output_dir, thm)
+    run_E_prover(selected_file, output_file, 60)
 
 
 if __name__ == "__main__":
-    chronology = Chronology("../data/chronology")
     problem_order = Problem_Order("../data/ProblemsInMMLOrder")
-    statements = Statements("../data/statements")
     problem_dir = "../problem/no_selection"
-    E_no_selection = "../E_output/no_selection"
-    Vampire_no_selection = "../Vampire_output/no_selection"
-    E_selection = "../E_output/E_selection"
-    Vampire_selection = "../Vampire_output/Vampire_selection"
-    if not os.path.exists(problem_dir):
-        os.mkdir(problem_dir)
-    if not os.path.exists(E_no_selection):
-        os.mkdir(E_no_selection)
-    if not os.path.exists(Vampire_no_selection):
-        os.mkdir(Vampire_no_selection)
-    if not os.path.exists(E_selection):
-        os.mkdir(E_selection)
-    if not os.path.exists(Vampire_selection):
-        os.mkdir(Vampire_selection)
-    Parallel(n_jobs=4)(delayed(process)(thm, statements, chronology,
-                                        problem_dir,
-                                        E_no_selection, E_selection,
-                                        Vampire_no_selection,
-                                        Vampire_selection)
-                       for thm in problem_order)
+    selected_problem_dir = "../problem/Vampire_selection"
+    output_dir = "../E_output/Vampire_selection"
+
+    if not os.path.exists(selected_problem_dir):
+        os.mkdir(selected_problem_dir)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    Parallel(n_jobs=10)(delayed(process)(thm, problem_dir,
+                                         selected_problem_dir,
+                                         output_dir)
+                        for thm in tqdm(problem_order))
