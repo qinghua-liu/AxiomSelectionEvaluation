1ee885807c1f34433866c2e9ade575d1f9252f46
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-09-04 11:04:39 -0400

    Update atp_with_selection.py

--- code/atp_with_selection.py
+++ code/atp_with_selection.py
@@ -11,7 +11,7 @@ PATH_TO_E = "/exp/home/tptp/Systems/E---2.5/eprover"
 PATH_TO_Vampire = "/exp/home/tptp/Systems/Vampire---4.5/vampire"
 
 def run_E_prover_with_selection(input_file, output_file, cpu_time):
-
+    # run E with SInE selection
     output = open(output_file, 'w+')
     subprocess.call([
         PATH_TO_E,
@@ -32,6 +32,7 @@ def run_E_prover_with_selection(input_file, output_file, cpu_time):
 
 
 def run_Vampire_prover_with_selection(input_file, output_file, cpu_time):
+    # run Vampire with selection
     output = open(output_file, 'w+')
     subprocess.call([
         PATH_TO_Vampire,
@@ -70,27 +71,27 @@ def run_E_prover_with_only_selection(input_file, output_file):
         stdout=output, stderr=open(os.devnull, "w"))
 
 
-def process(thm, problem_dir,
-            selected_problem_dir, output_dir):
-    input_file = os.path.join(problem_dir, thm)
-    selected_file = os.path.join(selected_problem_dir, thm)
-    run_Vampire_prover_with_only_selection(input_file, selected_file)
-    output_file = os.path.join(output_dir, thm)
-    run_E_prover(selected_file, output_file, 60)
+# def process(thm, problem_dir,
+#             selected_problem_dir, output_dir):
+#     input_file = os.path.join(problem_dir, thm)
+#     selected_file = os.path.join(selected_problem_dir, thm)
+#     run_Vampire_prover_with_only_selection(input_file, selected_file)
+#     output_file = os.path.join(output_dir, thm)
+#     run_E_prover(selected_file, output_file, 60)
 
 
-if __name__ == "__main__":
-    problem_order = Problem_Order("../data/ProblemsInMMLOrder")
-    problem_dir = "../problem/no_selection"
-    selected_problem_dir = "../problem/Vampire_selection"
-    output_dir = "../E_output/Vampire_selection"
+# if __name__ == "__main__":
+#     problem_order = Problem_Order("../data/ProblemsInMMLOrder")
+#     problem_dir = "../problem/no_selection"
+#     selected_problem_dir = "../problem/Vampire_selection"
+#     output_dir = "../E_output/Vampire_selection"
 
-    if not os.path.exists(selected_problem_dir):
-        os.mkdir(selected_problem_dir)
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+#     if not os.path.exists(selected_problem_dir):
+#         os.mkdir(selected_problem_dir)
+#     if not os.path.exists(output_dir):
+#         os.mkdir(output_dir)
 
-    Parallel(n_jobs=10)(delayed(process)(thm, problem_dir,
-                                         selected_problem_dir,
-                                         output_dir)
-                        for thm in tqdm(problem_order))
+#     Parallel(n_jobs=10)(delayed(process)(thm, problem_dir,
+#                                          selected_problem_dir,
+#                                          output_dir)
+#                         for thm in tqdm(problem_order))
