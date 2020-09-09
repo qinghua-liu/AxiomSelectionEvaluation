416c7a4e4789ee06dee6a552be65b1a26ffe9122
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-09-09 17:02:34 -0400

    Update selection.py

--- code/selection.py
+++ code/selection.py
@@ -1,3 +1,6 @@
+import os
+import csv
+
 from distance import distance_between_formulas, compute_noninfinity
 
 
@@ -44,5 +47,22 @@ def Qinf_selection(sorted_prem2score):
     scores = [pair[1] for pair in sorted_prem2score]
     prems = [pair[0] for pair in sorted_prem2score]
     non_inf_num = compute_noninfinity(scores)
+    selected_prems = prems[: non_inf_num]
+    return selected_prems
+
+
+def Qinf_selection_from_csv_ranking(thm, ranking_dir):
+    ranking_file = os.path.join(ranking_dir, thm)
+    with open(ranking_file, "r") as f:
+        csv_reader = csv.reader(f)
+        rows = [pair for pair in csv_reader]
+    prems = [row[0] for row in rows]
+    first_scores = [float(row[1].split(', ')[0].strip("[")) for row in rows]
+    second_scores = [float(row[1].split(', ')[1].strip("]")) for row in rows]
+    N = len(first_scores)
+    assert len(first_scores) == len(second_scores)
+    scores = [tuple([first_scores[i], second_scores[i]]) for i in range(N)]
+    non_inf_num = compute_noninfinity(
+        scores, tuple([float("inf"), float("inf")]))
     selected_prems = prems[: non_inf_num]
     return selected_prems
