c9972ff591e77ce9ebbb2aeddffd10f7aa2824e3
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-10-19 17:18:34 +0800

    Update test.py

--- code/test.py
+++ code/test.py
@@ -1,32 +1,9 @@
-import os
-from joblib import Parallel, delayed
-from tqdm import tqdm
-from data_structure import Problem_Order
-from atp import run_E_prover
-from atp_with_selection import run_E_prover_with_only_selection
+from data_structure import Proofs
+from evaluation import ranking_precision_in_pruney
 
 
-def process(thm, problem_dir,
-            selected_problem_dir, output_dir):
-    input_file = os.path.join(problem_dir, thm)
-    selected_file = os.path.join(selected_problem_dir, thm)
-    run_E_prover_with_only_selection(input_file, selected_file)
-    output_file = os.path.join(output_dir, thm)
-    run_E_prover(selected_file, output_file, 60)
+# pruney = Proofs("../data/new_pruney")
+# problem_dir = "/exp/home/qinghua/AxiomSelectionEvaluation-master/problem/E_selection"
 
-
-if __name__ == "__main__":
-    problem_order = Problem_Order("../data/ProblemsInMMLOrder")
-    problem_dir = "../problem/no_selection"
-    selected_problem_dir = "../problem/E_selection"
-    output_dir = "../E_output/E_selection"
-
-    if not os.path.exists(selected_problem_dir):
-        os.mkdir(selected_problem_dir)
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    Parallel(n_jobs=10)(delayed(process)(thm, problem_dir,
-                                         selected_problem_dir,
-                                         output_dir)
-                        for thm in tqdm(problem_order))
+# score = ranking_precision_in_pruney(pruney, problem_dir, "E")
+# print(score)
