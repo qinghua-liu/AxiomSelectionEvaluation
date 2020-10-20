ed13f5c906f57a6692d47cae84d167c7cb018f85
qinghua-liu <43528050+qinghua-liu@users.noreply.github.com>
2020-10-20 19:19:59 +0800

    Update test.py

--- code/test.py
+++ code/test.py
@@ -2,8 +2,8 @@ from data_structure import Proofs
 from evaluation import ranking_precision_in_pruney
 
 
-# pruney = Proofs("../data/new_pruney")
-# problem_dir = "/exp/home/qinghua/AxiomSelectionEvaluation-master/problem/E_selection"
+pruney = Proofs("../data/new_pruney")
+problem_dir = "/exp/home/qinghua/AxiomSelectionEvaluation-master/problem/E_selection"
 
-# score = ranking_precision_in_pruney(pruney, problem_dir, "E")
-# print(score)
+score = ranking_precision_in_pruney(pruney, problem_dir, "E")
+print(score)
